364480451964b5c757808bccbff125337a05895f
Mohammad Areeb Kashaf <areebkashaf7350@gmail.com>
2024-12-07 16:44:10 +0530

    using hive for local storage

--- lib/controllers/alarm_controller.dart
+++ lib/controllers/alarm_controller.dart
@@ -1,6 +1,30 @@
-import 'package:get/get.dart';
+import 'package:hive/hive.dart';
 import 'package:snooze_slayer/models/alarm_model.dart';
 
-class AlarmController extends GetxController {
-  var alarms = <Alarm>[].obs;
+class AlarmDBController {
+  var alarms = <Alarm>[];
+
+  Box<Alarm> alarmBox = Hive.box<Alarm>("alarms");
+
+  addCurrentAlarms() {
+    alarms = [
+      Alarm(
+        label: "Alarm 1",
+        time: "4:00 PM",
+        repeat: ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"],
+        missions: ["Math"],
+        snooze: Snooze(minutes: 5, times: 5),
+      ),
+      Alarm(
+        time: "6:00 PM",
+        repeat: ["SUN", "MON", "TUE"],
+      ),
+      Alarm(
+        time: "6:00 PM",
+      ),
+    ];
+    for (var alarm in alarms) {
+      alarmBox.add(alarm);
+    }
+  }
 }
